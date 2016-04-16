51a3d6040b8427ad3460c8968f5db82a8c22f62c
CAOLEI <183515951@qq.com>
2016-04-16 22:15:20 +0800

    init

---
+++ PinYinConv/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using PinYinConv.src.util;
+
+namespace PinYinConv
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private delegate void setListBoxAnswerGate(string str);
+        private void setListBoxAnswer(string str)
+        {
+            if (ListBoxAnswer.Dispatcher.Thread != Thread.CurrentThread)
+            {
+                setListBoxAnswerGate sg = new setListBoxAnswerGate(setListBoxAnswer);
+                Dispatcher.Invoke(sg, new object[] { str });
+            }
+            else
+            {
+                ListBoxAnswer.Items.Add(str);
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonFirst_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxAnswer.Items.Clear();
+            string str = TextBoxCh.Text;
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return;
+            }
+            string[] strs = PinYinUtils.strToFirst(str);
+            foreach (string s in strs)
+            {
+                setListBoxAnswer(s);
+            }
+        }
+
+        private void ButtonFull_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxAnswer.Items.Clear();
+            string str = TextBoxCh.Text;
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return;
+            }
+            string[] strs = PinYinUtils.strToFull(str);
+            foreach (string s in strs)
+            {
+                setListBoxAnswer(s);
+            }
+        }
+    }
+}
---
+++ PinYinConv/src/util/PinYinUtils.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.International.Converters.PinYinConverter;
+
+namespace PinYinConv.src.util
+{
+    public static class PinYinUtils
+    {
+       
+        private static string[] charToFull(char ch, ref string error)
+        {
+            try
+            {
+                HashSet<string> purePinYinSet = new HashSet<string>();
+                ChineseChar chChar = new ChineseChar(ch);
+                for (int i = 0; i < chChar.PinyinCount; i++)
+                {
+                    string tonePinYin = chChar.Pinyins[i].ToString();
+                    string purePinYin = tonePinYin.Substring(0, tonePinYin.Length - 1);
+                    purePinYinSet.Add(purePinYin);
+                }
+                return purePinYinSet.ToArray<string>();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return null;
+        }
+        private static string[] charToFirst(char ch, ref string error)
+        {
+            try
+            {
+                HashSet<string> purePinYinSet = new HashSet<string>();
+                ChineseChar chChar = new ChineseChar(ch);
+                for (int i = 0; i < chChar.PinyinCount; i++)
+                {
+                    string tonePinYin = chChar.Pinyins[i].ToString();
+                    string purePinYin = tonePinYin.Substring(0, 1);
+                    purePinYinSet.Add(purePinYin);
+                }
+                return purePinYinSet.ToArray<string>();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return null;
+        }
+        private static string[][] getStrsArray(string strLine)
+        {
+            List<string[]> strsList = new List<string[]>();
+            
+            foreach (char ch in strLine)
+            {
+                string error = null;
+                string[] strs = charToFull(ch,ref error);
+                if (error == null)
+                {
+                    strsList.Add(strs);
+                }
+                else
+                {
+                    char cc = char.ToUpper(ch);
+                    string ss = cc.ToString();
+                    if (!string.IsNullOrWhiteSpace(ss))
+                    {
+                        strsList.Add(new string[] { ss });
+                    }
+                }
+            }
+            return strsList.ToArray<string[]>();
+        }
+        private static string[][] getStrsArrayFirst(string strLine)
+        {
+            List<string[]> strsList = new List<string[]>();
+
+            foreach (char ch in strLine)
+            {
+                string error = null;
+                string[] strs = charToFirst(ch, ref error);
+                if (error == null)
+                {
+                    strsList.Add(strs);
+                }
+                else
+                {
+                    char cc = char.ToUpper(ch);
+                    string ss = cc.ToString();
+                    if (!string.IsNullOrWhiteSpace(ss))
+                    {
+                        strsList.Add(new string[] { ss });
+                    }
+                }
+            }
+            return strsList.ToArray<string[]>();
+        }
+        private static void mixStrsArray(ref string[][] strsArray, ref string[] strs, int index)
+        {
+            if (index >= strsArray.Length)
+            {
+                return;
+            }
+            if (index == 0)
+            {
+                int l = strsArray[0].Length;
+                strs = new string[l];
+                for (int i = 0; i < l; i++)
+                {
+                    strs[i] = strsArray[0][i];
+                }
+                index++;
+                if (index >= strsArray.Length)
+                {
+                    return;
+                }
+            }
+            int strsLength = strsArray[index].Length;
+            string[] newStrs = new string[strs.Length * strsLength];
+            int newIndex = 0;
+            for (int i = 0; i < strsLength; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    newStrs[newIndex] = strs[j] + strsArray[index][i];
+                    newIndex++;
+                }
+            }
+            strs = newStrs;
+            mixStrsArray(ref strsArray, ref strs, ++index);
+        }
+        public static string[] strToFull(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return new string[0];
+            }
+            string[][] strsArray = getStrsArray(str);
+            string[] strs = null;
+            mixStrsArray(ref strsArray, ref strs, 0);
+            return strs;
+        }
+        public static string[] strToFirst(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return new string[0];
+            }
+            string[][] strsArray = getStrsArrayFirst(str);
+            string[] strs = null;
+            mixStrsArray(ref strsArray, ref strs, 0);
+            return strs;
+        }
+        
+    }
+} 
